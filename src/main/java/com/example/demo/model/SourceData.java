90713301f1e2c7e026b8c11d400664f42ff143fe
armenaslikyan86 <armenaslikyan@gmail.com>
2023-04-04 23:26:38 +0400

    stupid commit

--- src/main/java/com/example/demo/model/SourceData.java
+++ src/main/java/com/example/demo/model/SourceData.java
@@ -1,6 +1,12 @@
 package com.example.demo.model;
 
-/
+import org.apache.pulsar.shade.org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.pulsar.shade.org.apache.commons.lang.builder.HashCodeBuilder;
+
+/**
+ * Created by armen aslikyan on 10/11/2017.
+ * This class is used to store source data
+ */
 public class SourceData {
 
     private String sourceField;
@@ -11,5 +17,31 @@ public class SourceData {
 
     public void setSourceField(String sourceField) {
         this.sourceField = sourceField;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SourceData that = (SourceData) o;
+
+        return new EqualsBuilder()
+                .append(sourceField, that.sourceField)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(sourceField).toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "SourceData{" +
+                "sourceField='" + sourceField + '\'' +
+                '}';
     }
 }
