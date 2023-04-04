1d0d5b80ce6eb63ad03057bbcd3d0be45298b97c
armenaslikyan86 <armenaslikyan@gmail.com>
2023-04-04 23:25:21 +0400

    fake commit

---
+++ src/main/java/com/example/demo/model/SourceData.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+/
+public class SourceData {
+
+    private String sourceField;
+
+    public String getSourceField() {
+        return sourceField;
+    }
+
+    public void setSourceField(String sourceField) {
+        this.sourceField = sourceField;
+    }
+}
